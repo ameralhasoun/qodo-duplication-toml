63ff1ba843a4947ec18ed5989f3b55f9b11ac62f
Amer Alhasoun <a.alhasoun@viscon.eu>
2025-10-23 10:46:52 +0200

    add rekenmachine

--- Calculator.cs
+++ Calculator.cs
@@ -1,7 +1,15 @@
-    public class Calculator
+public class Calculator
+{
+    public int AddNumbers(int a, int b)
     {
-        public int AddNumbers(int a, int b)
-        {
-            return a + b;
-        }
+        return a + b;
     }
+}
+    
+public class Rekenmachine
+{
+    public int Optellen(int a, int b)
+    {
+        return a + b;
+    }
+}
